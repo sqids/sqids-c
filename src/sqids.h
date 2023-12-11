746f134a4f7b38b68c32c7681c40968d04af5b09
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2023-12-12 01:10:01 +0200

    Make versioning easier and tag 0.2.0

--- src/sqids.h
+++ src/sqids.h
@@ -5,9 +5,9 @@
 /* {{{ version information                                                   */
 /*****************************************************************************/
 
-#define SQIDS_VERSION_STRING "0.1.0"
+#define SQIDS_VERSION_STRING "0.2.0"
 #define SQIDS_VERSION_MAJOR 0
-#define SQIDS_VERSION_MINOR 1
+#define SQIDS_VERSION_MINOR 2
 #define SQIDS_VERSION_PATCH 0
 
 /* }}}                                                                       */
